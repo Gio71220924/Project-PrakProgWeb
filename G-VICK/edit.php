70961f4b3562694d62787fbbf12fbe58c205da69
Giovanka Steviano Harry Premono <giovanka.steviano@ti.ukdw.ac.id>
2024-06-09 11:29:12 +0700

    Form edit dan CSS nya

---
+++ G-VICK/edit.php
@@ -0,0 +1,30 @@
+<?php
+    session_start();
+    require 'koneksi.php';
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style-pesan.css" />
+    <link href="https://fonts.googleapis.com/css?family=Inter" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" />
+    <link rel="icon" type="images/icon.png" href="images/icon.png">
+    <title>Website Pemesanan Tiket G-VicxID</title>
+    <style>
+        <?php
+            include 'edit.css';
+        ?>
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
